a3fefd0d359d67acd39e1f9902c96500e11abab1
feanorcf <m.taylan.k28@gmail.com>
2024-04-15 20:21:35 +0300

    support for any different os

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,25 @@ public class Main {
 		int num_of_questions = answers[0] + answers[1];
 		int correct_answers = answers[0];
 		
-		double result = calculatePoints(JUnitTests.MAX_POINTS, num_of_questions, correct_answers);
+		int result = (int)Math.round(calculatePoints(JUnitTests.MAX_POINTS, num_of_questions, correct_answers));
 		
-		System.out.println(result + "/" + JUnitTests.MAX_POINTS);
+		System.out.println("Result: " + result + "/" + JUnitTests.MAX_POINTS);
 	}
 	
 	// returns an integer array which includes successfull answers(0) and failed answers(1) count
 	public static int[] getTestResults() {
 		File location = new File(".");
 		
-		String compile_command = "javac -d bin -sourcepath src -cp .:lib/junit-platform-console-standalone-1.10.2.jar src/JUnitTests.java";
+		String compile_command = "";
 		String run_command = "java -jar lib/junit-platform-console-standalone-1.10.2.jar execute -cp bin/ -c JUnitTests";
+		
+		// use ";" instead of ":" on windows
+		if(Terminal.IS_WINDOWS) {
+			compile_command = "javac -d bin -sourcepath src -cp .;lib/junit-platform-console-standalone-1.10.2.jar src/JUnitTests.java";
+		}
+		else {
+			compile_command = "javac -d bin -sourcepath src -cp .:lib/junit-platform-console-standalone-1.10.2.jar src/JUnitTests.java";			
+		}
 		
 		try {
 			Terminal.run(location, compile_command);
